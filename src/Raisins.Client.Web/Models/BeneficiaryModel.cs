7ce36a9222184b0f0ad5c92428fede2782a14268
Marc Vitalis <marc.vitalis@live.com>
2010-11-08 21:09:03 +0800

    updated model

--- src/Raisins.Client.Web/Models/BeneficiaryModel.cs
+++ src/Raisins.Client.Web/Models/BeneficiaryModel.cs
@@ -24,14 +24,31 @@ namespace Raisins.Client.Web.Models
 		public long ID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public int Votes { get; set; }
-		public float TotalAmount { get; set; }
+		public BeneficiaryDetail Detail { get; set; }
 	
 		#endregion
 
-        #region Operations
+		#region Operations
 
-        public static BeneficiaryModel[] GetStatistics()
+		public static BeneficiaryModel[] GetStatistics()
+		{
+            Beneficiary[] beneficiaries = Beneficiary.FindAll();
+
+			List<BeneficiaryModel> models = new List<BeneficiaryModel>();
+
+			foreach (Beneficiary beneficiary in beneficiaries)
+			{
+				models.Add(ToModel(beneficiary));
+
+                BeneficiaryDetail detail = new BeneficiaryDetail();
+                detail.TotalAmount = beneficiary.Accounts.Sum(account => account.Amount);
+                detail.Votes = 0;
+			}
+
+			return models.ToArray();
+		}
+
+        public static BeneficiaryModel[] FindAll()
         {
             Beneficiary[] beneficiaries = Beneficiary.FindAll();
 
@@ -42,37 +59,43 @@ namespace Raisins.Client.Web.Models
                 models.Add(ToModel(beneficiary));
             }
 
-            //sort
             return models.ToArray();
         }
 
-        #endregion
+		#endregion
 
-        #region Helper methods
+		#region Helper methods
 
-        protected static BeneficiaryModel ToModel(Beneficiary data)
-        {
-            BeneficiaryModel model = new BeneficiaryModel();
-            model.Description = data.Description;
-            model.ID = data.ID;
-            model.Name = data.Name;
-            model.TotalAmount = data.Accounts.Sum(account => account.Amount);
-            model.Votes = 0;
+		protected static BeneficiaryModel ToModel(Beneficiary data)
+		{
+			BeneficiaryModel model = new BeneficiaryModel();
+			model.Description = data.Description;
+			model.ID = data.ID;
+			model.Name = data.Name;
 
-            return model;
-        }
+			return model;
+		}
 
-        protected static Beneficiary ToData(BeneficiaryModel model)
-        {
-            Beneficiary data = new Beneficiary();
-            data.Description = model.Description;
-            data.ID = model.ID;
-            data.Name = model.Name;
+		protected static Beneficiary ToData(BeneficiaryModel model)
+		{
+			Beneficiary data = new Beneficiary();
+			data.Description = model.Description;
+			data.ID = model.ID;
+			data.Name = model.Name;
 
-            return data;
-        }
+			return data;
+		}
 
-        #endregion
+		#endregion
 
 	}
+
+	public class BeneficiaryDetail
+	{
+
+		public float TotalAmount { get; set; }
+		public int Votes { get; set; }
+
+	}
+
 }
