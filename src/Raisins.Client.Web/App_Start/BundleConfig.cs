6f790b285440301c9eda969f6e0ee4a7f24be965
Marc Vitalis <marc.vitalis@live.com>
2012-10-31 18:43:12 +0800

    added bundles for javascript

--- src/Raisins.Client.Web/App_Start/BundleConfig.cs
+++ src/Raisins.Client.Web/App_Start/BundleConfig.cs
@@ -12,7 +12,10 @@ namespace Raisins.Client.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/reset.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/content/css").Include("~/Content/reset.css", "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/scripts/knockout").Include("~/Scripts/knockout-2.2.0.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery").Include("~/Scripts/jquery-*"));
+            bundles.Add(new ScriptBundle("~/scripts/raisins").Include("~/Scripts/kkcountdown.js", "~/Scripts/raisins.js"));
 
         }
 
---
+++ src/Raisins.Client.Web/Scripts/raisins.js
@@ -0,0 +1,21 @@
+﻿$().ready(function () {
+    $("ul#teamscontent li a").hover(
+        function () {
+            $(this).parent().find('h4').show("slide", { direction: "down", easing: 'easeOutBounce' });
+        },
+        function () {
+            $(this).parent().find('h4').hide("slide", { direction: "down", easing: 'easeOutBounce' });
+        }
+
+    );
+
+    $("#timer").kkcountdown({
+        dayText: ':',
+        daysText: ':',
+        hoursText: ':',
+        minutesText: ':',
+        secondsText: '',
+        displayZeroDays: false,
+        oneDayClass: 'one-day'
+    });
+});
