bade073e990cdc5c1b3d723b24d3c01647c35e0e
theo salvador <xtian_oeht@yahoo.com>
2012-11-08 13:30:01 +0800

    added band member profiles

--- src/Raisins.Client.Web/Scripts/raisins.js
+++ src/Raisins.Client.Web/Scripts/raisins.js
@@ -18,4 +18,16 @@
         displayZeroDays: false,
         oneDayClass: 'one-day'
     });
+    equalHeight($("ul#bandmembers li"));
+
+    function equalHeight(group) {
+        var tallest = 0;
+        group.each(function () {
+            var thisHeight = $(this).height();
+            if (thisHeight > tallest) {
+                tallest = thisHeight;
+            }
+        });
+        group.height(tallest);
+    }
 });
