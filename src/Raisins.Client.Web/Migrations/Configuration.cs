8161ee8ce3334bb1bb3acee327eab201b300c551
Marc Vitalis <marc.vitalis@live.com>
2013-11-26 18:44:10 +0800

    flatten database

--- src/Raisins.Client.Web/Migrations/Configuration.cs
+++ src/Raisins.Client.Web/Migrations/Configuration.cs
@@ -1,55 +1,31 @@
 namespace Raisins.Client.Web.Migrations
 {
-    using Raisins.Client.Web.Models;
     using System;
-    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Data.Entity.Migrations.Infrastructure;
-    using System.Diagnostics;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<RaisinsDB>
+    internal sealed class Configuration : DbMigrationsConfiguration<Raisins.Client.Web.Models.RaisinsDB>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(RaisinsDB context)
+        protected override void Seed(Raisins.Client.Web.Models.RaisinsDB context)
         {
-            //Seeder.Seed(context);
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
     }
-
-    //public sealed class Configuration : DbMigrationsConfiguration<RaisinsDB>
-    //{
-    //    public Configuration()
-    //    {
-    //        AutomaticMigrationsEnabled = false;
-    //    }
-
-    //    protected override void Seed(RaisinsDB context)
-    //    {
-    //       // Seeder.Seed(context);
-    //        Configuration configuration = new Configuration();
-    //    configuration.ContextType = typeof(RaisinsDB);
-    //    var migrator = new DbMigrator(configuration);
-
-    //    //This will get the SQL script which will update the DB and will write it to debug
-    //    var scriptor = new MigratorScriptingDecorator(migrator);
-    //    string script = scriptor.ScriptUpdate(sourceMigration: null, targetMigration: null).ToString();
-    //    Debug.Write(script);
-
-    //    //This will run the migration update script and will run Seed() method
-    //    migrator.Update();
-    //    } 
-        
-       
-    //}
-
-        
-
-   
 }
-
