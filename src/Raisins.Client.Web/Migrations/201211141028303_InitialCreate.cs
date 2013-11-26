c89fbd805d1959764b9ac1474e45b04598f1f914
romedison <romedison_reyes@yahoo.com>
2013-11-26 09:20:27 +0800

    Edited

--- src/Raisins.Client.Web/Migrations/201211141028303_InitialCreate.cs
+++ src/Raisins.Client.Web/Migrations/201211141028303_InitialCreate.cs
@@ -59,7 +59,7 @@ namespace Raisins.Client.Web.Migrations
                         ExchangeRate = c.Decimal(nullable: false, precision: 18, scale: 2),
                     })
                 .PrimaryKey(t => t.ID);
-            
+
             CreateTable(
                 "Payments",
                 c => new
@@ -77,9 +77,9 @@ namespace Raisins.Client.Web.Migrations
                         CurrencyID = c.Int(nullable: false),
                         CreatedByID = c.Int(nullable: false),
                         AuditedByID = c.Int(),
-                        //starts here
-                        CreatedDate=c.DateTime(nullable:false),
-                        ModifiedDate = c.DateTime(nullable: false),
+                        ////starts here
+                        //CreatedDate=c.DateTime(nullable:false),
+                        //ModifiedDate = c.DateTime(nullable: false),
                     })
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("Beneficiaries", t => t.BeneficiaryID, cascadeDelete: true)
@@ -91,10 +91,10 @@ namespace Raisins.Client.Web.Migrations
                 .Index(t => t.ExecutiveID)
                 .Index(t => t.CurrencyID)
                 .Index(t => t.CreatedByID)
-                .Index(t => t.AuditedByID)
-                //startshere
-                .Index(t => t.CreatedDate)
-                .Index(t => t.ModifiedDate);
+                .Index(t => t.AuditedByID);
+                ////startshere
+                //.Index(t => t.CreatedDate)
+                //.Index(t => t.ModifiedDate);
             
             CreateTable(
                 "Tickets",
