9e2b065dfe3e24e116e2ca63a64ee6861ab44104
Danica Delos Reyes <delosreyes_dan@yahoo.com>
2013-11-29 18:57:15 +0800

    try fhms mailhost

--- src/Raisins.Client.Web/Models/Payment.cs
+++ src/Raisins.Client.Web/Models/Payment.cs
@@ -309,7 +309,7 @@ namespace Raisins.Client.Web.Models
                 message.Subject = "[TALENTS FOR HUNGRY MINDS 2013] Ticket Notification";
                 message.IsBodyHtml = true;
 
-                SmtpClient smtp = new SmtpClient("mailhost.navitaire.com");
+                SmtpClient smtp = new SmtpClient("10.78.160.16", 25);
                 //smtp.Credentials = new System.Net.NetworkCredential("talentsforhungryminds2013@gmail.com","fhms2013");
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
 
