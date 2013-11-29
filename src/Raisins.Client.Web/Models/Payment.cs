ad775ea37e8e03bab5d8f73a2e6193b1b092f8b5
Danica Delos Reyes <delosreyes_dan@yahoo.com>
2013-11-29 18:20:00 +0800

    try gmail

--- src/Raisins.Client.Web/Models/Payment.cs
+++ src/Raisins.Client.Web/Models/Payment.cs
@@ -304,16 +304,17 @@ namespace Raisins.Client.Web.Models
 
             try
             {
-                MailMessage message = new MailMessage("no-reply@navitaire.com", email);
+                MailMessage message = new MailMessage("talentsforhungryminds2013@gmail.com", email);
                 message.Body = content;
                 message.Subject = "[TALENTS FOR HUNGRY MINDS 2013] Ticket Notification";
                 message.IsBodyHtml = true;
 
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "fhms";
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 465);
+                smtp.Credentials = new System.Net.NetworkCredential("talentsforhungryminds2013@gmail.com","fhms2013");
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
                 smtp.UseDefaultCredentials = false;
-                smtp.Port = 25;
+
                 smtp.Send(message);
             }
             catch { }
