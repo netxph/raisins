40d6cd6ec2d65322b1801b4b7469ffe6326cbce7
Marc Vitalis <marc.vitalis@live.com>
2013-12-04 15:29:17 +0800

    add delegate email job

---
+++ Raisins.Client.MailJob/Mailer.cs
@@ -0,0 +1,63 @@
+﻿using Raisins.Client.Web.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raisins.Client.MailJob
+{
+    public class Mailer
+    {
+
+
+        public Mailer()
+        {
+            Settings = new Settings();
+        }
+
+        protected Settings Settings { get; set; }
+
+        public void Run()
+        {
+            List<MailQueue> mails = DequeueMails(); 
+            
+            while(mails != null && mails.Count > 0)
+            {
+                Send(mails);
+
+                mails = DequeueMails();
+            }
+        }
+
+        public List<MailQueue> DequeueMails()
+        {
+            var client = new RestClient(Settings.ServiceBaseUrl);
+            var request = new RestRequest("mailer", Method.GET);
+
+            var response = client.Execute<List<MailQueue>>(request);
+
+            return response.Data;
+        }
+
+        public void Send(List<MailQueue> mails)
+        {
+            
+            var smtp = new SmtpClient(Settings.SmtpHost, Settings.SmtpPort);
+
+            foreach(var mail in mails)
+            {
+                var message = new MailMessage(mail.From, mail.To);
+                message.Body = mail.Content;
+                message.Subject = mail.Subject;
+                message.IsBodyHtml = true;
+
+                smtp.Send(message);
+            }
+        
+        }
+
+    }
+}
