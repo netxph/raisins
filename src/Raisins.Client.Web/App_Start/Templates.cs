b31f196d80356d02e3ac1d9795663d6cdc1bedc0
Danica Delos Reyes <delosreyes_dan@yahoo.com>
2014-12-08 13:51:51 +0800

    update email template vote end period

--- src/Raisins.Client.Web/App_Start/Templates.cs
+++ src/Raisins.Client.Web/App_Start/Templates.cs
@@ -20,7 +20,7 @@ Ticket Number(s) for your vote(s) is/are:<br /><br />
 </p>
 <p style=""font-size: 17px;font-family:'Calibri'"">Let's beat our record last year and raise more than Php 600,000 for scholars of the Food for Hungry Minds.</p>
 <p style=""font-size: 17px;font-family:'Calibri'"">All votes will be part of the raffle draw (1 vote = 1 entry).</p>
-<p style=""font-size: 17px;font-family:'Calibri'"">Voting period ends on December 19, 2014.</p>
+<p style=""font-size: 17px;font-family:'Calibri'"">Voting period ends on December 17, 2014.</p>
 <br />
 <p style=""font-size: 18px;font-family:'Calibri'""><strong>'SUS, NAV-Pasikat' Fundraising 2014</strong><br />
 <a href=""http://foodforhungryminds.org/"">http://foodforhungryminds.org</a>
