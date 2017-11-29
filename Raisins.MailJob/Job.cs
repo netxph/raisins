5f2e9a29f35c4b6ee2a0ec13bf2e891d67ad458b
Marc Abraham VITALIS <marc-abraham.vitalis@amadeus.com>
2017-11-29 15:26:41 +0800

    add mailjob

---
+++ Raisins.MailJob/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Raisins.MailJob
+{
+    public class Job
+    {
+        
+        public int Count { get; private set; }
+        public int Interval { get; private set; }
+
+        public Job(int count, int interval)
+        {
+            Count = count;
+            Interval = interval;
+        }
+
+        public void Run(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine("Hello world!!!");
+            }
+        }
+    }
+}
---
+++ Raisins.MailJob/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Raisins.MailJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenSource = new CancellationTokenSource();
+            var job = new Job(10, 10000);
+
+            var task = Task.Factory.StartNew(() => job.Run(tokenSource.Token));
+
+            if (Console.ReadKey().Key == ConsoleKey.Q)
+            {
+                tokenSource.Cancel();
+
+                try
+                {
+                    task.Wait();
+                }
+                catch (AggregateException ae)
+                {
+                    foreach (var ex in ae.InnerExceptions)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                finally
+                {
+                    tokenSource.Dispose();
+                }
+            }
+
+            
+        }
+    }
+}
