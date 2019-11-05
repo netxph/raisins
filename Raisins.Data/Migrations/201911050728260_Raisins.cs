265b0061ca2e317190333a50f6eabea584ca238c
WhilceDy <55231318+WhilceDy@users.noreply.github.com>
2019-11-05 17:03:56 +0800

    Fixed some Bugs

---
+++ Raisins.Data/Migrations/201911050728260_Raisins.cs
@@ -0,0 +1,16 @@
+namespace Raisins.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Raisins : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
